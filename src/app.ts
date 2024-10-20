ee574ba3cdfc440937f4693f60453cbc124e2ed5
Ronoyaro <ronaldferreiraig@hotmail.com>
2024-10-20 20:10:12 -0300

    criei as rotas do DELETE dos medicamentos

--- src/app.ts
+++ src/app.ts
@@ -3,8 +3,8 @@ import express from "express";
 const app = express();
 
 interface Medicine {
-  id: number,
-  tradeName: string; 
+  id: number;
+  tradeName: string;
   activeIngredient: string;
   dosage: number;
   dosageUnit: "mg" | "ml" | "mcg";
@@ -13,35 +13,49 @@ interface Medicine {
 }
 
 const medicines: Medicine[] = [];
+let currentId: number = 1;
 
-app.use(express.json())
+app.use(express.json());
 
-app.get('/medicines', (req, res) => {
-  res.status(200).json(medicines)
-})
+app.get("/medicines", (req, res) => {
+  res.status(200).json(medicines);
+});
 
-app.get('/medicines/:id', (req, res) => {
-  const id = Number(req.params.id)
+app.get("/medicines/:id", (req, res) => {
+  const id = Number(req.params.id);
   const medicine = medicines.find((medicine) => medicine.id === id);
 
-  if(!medicine){
-    res.status(404).json({message: 'Medicine not found'})
+  if (!medicine) {
+    res.status(404).json({ message: "Medicine not found" });
     return;
-  } 
-   res.status(200).json(medicine)
-})
-
-app.post('/medicines', (req, res) => {
-  const medicine: Medicine = {
-    id: medicines.length + 1,
-    ...req.body
   }
-  
-  medicines.push(medicine)
-  
-  res.status(201).json(medicine)
-})
+  res.status(200).json(medicine);
+});
+
+app.post("/medicines", (req, res) => {
+  const medicine: Medicine = {
+    id: currentId,
+    ...req.body,
+  };
+  currentId++;
+
+  medicines.push(medicine);
+
+  res.status(201).json(medicine);
+});
+
+app.delete("/medicines/:id", (req, res) => {
+  const id = Number(req.params.id);
+  const index = medicines.findIndex((medicine) => medicine.id === id);
+  if (index === -1) {
+    res.status(404).json({ message: "Medicine not found" });
+    return;
+  }
+  const medicine = medicines[index]; //retornando o objeto que foi compativel com o id que ele encontrou;
+  medicines.splice(index, 1); //Deleta o elemento da Array
+  res.status(200).json(medicine);
+});
 
 app.listen(3000, () => {
-  console.log('server running on port 3000')
-})
+  console.log("server running on port 3000");
+});
