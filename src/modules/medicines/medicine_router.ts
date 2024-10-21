debdaf2a8eed6e6b6406b4746d4cf8813082411e
Ronoyaro <ronaldferreiraig@hotmail.com>
2024-10-21 20:47:10 -0300

    criei uma classe para chamar minhas funções de maneira mais ludica

--- src/modules/medicines/medicine_router.ts
+++ src/modules/medicines/medicine_router.ts
@@ -1,14 +1,12 @@
 import { Router } from "express";
-import { createMedicine, deleteMedicine, findMedicineById, listMedicines } from "./medicine_controller";
+import { MedicineController } from "./medicine_controller";
+
 
 const medicineRouter = Router();
 
-medicineRouter.get("/", listMedicines);
-
-medicineRouter.get("/:id", findMedicineById);
-
-medicineRouter.post("/", createMedicine);
-
-medicineRouter.delete("/:id", deleteMedicine);
+medicineRouter.get("/", MedicineController.list);
+medicineRouter.get("/:id", MedicineController.findById);
+medicineRouter.post("/", MedicineController.create);
+medicineRouter.delete("/:id", MedicineController.delete);
 
 export { medicineRouter };
